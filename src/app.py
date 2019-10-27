56c677c7078b47a00df3bd37aaaa0ee076b2a00e
Ray Ng <ray.ng@gradeous.com.au>
2019-10-27 23:24:23 +1100

    add timer

--- src/app.py
+++ src/app.py
@@ -1,14 +1,50 @@
 #! /usr/bin/env python
 import os
-from flask import Flask, jsonify, request
+import re
+from flask import Flask, render_template, url_for, redirect, request, flash, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '123456!234'
+
 CORS(app)
 
 @app.route('/')
 def index():
-    return "<h3> Consul Mesh Gateway Multi-Cloud Demo <h3>"
+    return redirect(url_for('timer', num=25*60))
+
+@app.route('/<int:num>s')
+@app.route('/<int:num>')
+def timer(num):
+    return render_template('index.html', num=num)
+
+@app.route('/custom', methods=['GET', 'POST'])
+def custom():
+    time = request.form.get('time', 180)
+    # use re to validate input data
+    m = re.match('\d+[smh]?$', time)
+    if m is None:
+        flash(u'please enter a time 34、20s、15m、2h')
+        return redirect(url_for('index'))
+    if time[-1] not in 'smh':
+        return redirect(url_for('timer', num=int(time)))
+    else:
+        type = {'s': 'timer', 'm': 'minutes', 'h': 'hours'}
+        return redirect(url_for(type[time[-1]], num=int(time[:-1])))
+
+@app.route('/<int:num>m')
+def minutes(num):
+    return redirect(url_for('timer', num=num*60))
+
+
+@app.route('/<int:num>h')
+def hours(num):
+    return redirect(url_for('timer', num=num*3600))
+
+# todo pomodoro mode: loop a 25-5 minutes cycle
+@app.route('/pomodoro')
+def pomodoro():
+    return render_template('index.html')
 
 @app.route("/health")
 def health():
